68c130bf448a780d4c904005396d4b9f332e6eac
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-07-06 12:33:59 +0200

    delete Event

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,22 +1,36 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
 import Header from "../Header.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+      });
+      navigate("/events");
+    },
+  });
   const urlImg = "http://localhost:3000/";
-  const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+  const formattedDate = new Date(data?.date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "short",
     year: "numeric",
   });
+  function handleClick() {
+    mutate({ id });
+  }
   return (
     <>
       <Outlet />
@@ -25,29 +39,29 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      {isPending && (
-        <div id="event-details-content" className="center">
-          <p>Fetching event data...</p>
-        </div>
-      )}
-      {isError && (
-        <div id="event-details-content" className="center">
-          <ErrorBlock
-            title="Failed to load event."
-            message={
-              error.info?.message ||
-              "Failed to fetch event data, please try again leter."
-            }
-          />
-        </div>
-      )}
       <article id="event-details">
+        {isPending && (
+          <div id="event-details-content" className="center">
+            <p>Fetching event data...</p>
+          </div>
+        )}
+        {isError && (
+          <div id="event-details-content" className="center">
+            <ErrorBlock
+              title="Failed to load event."
+              message={
+                error.info?.message ||
+                "Failed to fetch event data, please try again leter."
+              }
+            />
+          </div>
+        )}
         {data && (
           <>
             <header>
               <h1>{data.title}</h1>
               <nav>
-                <button>Delete</button>
+                <button onClick={handleClick}>Delete</button>
                 <Link to="edit">Edit</Link>
               </nav>
             </header>
